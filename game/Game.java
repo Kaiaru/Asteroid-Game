c81df22320031e05bde1cabfa977f7299cb0f48b
Charlie Lin <charlielin7@gmail.com>
2016-02-18 13:30:23 -0500

    first commit

---
+++ game/Game.java
@@ -0,0 +1,26 @@
+package game;
+
+public class Game {
+
+	int score = 0;
+	
+	public static void main(String args[]){
+		
+	}
+	
+	public static void updateDyanmicLogic(){
+		//if (event.getKeyCode() == KeyEvent. VK_UP)
+	}
+	
+	public static void updateFixedLogic(){
+		
+	}
+	
+	public static void mainLoop(){
+		
+	}
+	
+	public static void repaint(){
+		
+	}
+}
---
+++ game/Rock.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Rock {
+
+	boolean isRock;
+	boolean isShip;
+	
+	Rock(boolean ship, boolean rock){
+		isShip = ship;
+		isRock = rock;
+	}
+}
