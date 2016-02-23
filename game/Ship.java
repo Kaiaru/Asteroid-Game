bad0ecaa085e6c5f978a7228bbeec900d150648b
Kaiaru <kfedx@udel.edu>
2016-02-23 00:30:46 -0500

    Added Bombs

--- game/Ship.java
+++ game/Ship.java
@@ -5,10 +5,21 @@ import java.lang.Math;
 public class Ship extends SpaceObj {
 //Ship is a SpaceObj that represents the player's ship
 
+	private boolean hasBomb;
+
 	Ship(int width) {
 		printOut = "^";
 		xcor = width/2;
 		ycor = 1;
+		hasBomb = false;
+	}
+	
+	public boolean getBomb(){
+		return hasBomb;
+	}
+	
+	public void setBomb(boolean x){
+		hasBomb = x;
 	}
 
 }
